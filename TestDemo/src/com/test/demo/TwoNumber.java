842711221b9cdffe1bfa5b0e7ff67e33dc8499f3
user <user@DESKTOP-UIAKQDD>
2022-12-14 19:06:36 +0530

    First Commit

---
+++ TestDemo/src/com/test/demo/TwoNumber.java
@@ -0,0 +1,17 @@
+package com.test.demo;
+
+import java.util.*;
+
+public class TwoNumber {
+    
+    public static void main(String[] args) {
+        
+        int x = 89; 
+        
+        int y = 43;
+        
+        int sum = x + y;
+        
+        System.out.println("Sum is " + sum);
+    }
+}
